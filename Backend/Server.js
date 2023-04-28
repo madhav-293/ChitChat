51aa2bb4134cd0b64f7bc6adfd2a5ceef08e00a5
MADHAV MAHESHWARI <madhavm774@gmail.com>
2023-04-28 10:28:43 +0530

    Updates

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,25 @@
 const express =require('express')
-
+const dotenv =require('dotenv')
+const { chats } = require('./data/Data')
 const app=express()
+dotenv.config();
 
 app.get('/',(req,res)=>{
     res.send('API is running')
+});
+
+app.get("/api/chat",(req,res)=>{
+    // res.send("Hello")
+    res.send(chats);
+    // console.log(chats)
+});
+
+const PORT=process.env.PORT || 5000
+
+app.get('/api/chat/:id',(req,res)=>{
+    // console.log(req.params.id)
+    const singleChat=chats.find((c)=>c._id==req.params.id);
+    res.send(singleChat);
 })
 
-app.get('/')
-
-app.listen(5000,console.log('Server Started on Port : 5000'))
+app.listen(PORT,console.log(`Server Started on Port : ${PORT}`));
