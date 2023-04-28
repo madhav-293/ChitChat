2e5cf30606feddd241fb9cd058277c9ed52d2ef7
MADHAV MAHESHWARI <madhavm774@gmail.com>
2023-04-29 00:07:46 +0530

    Login and Signup Updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import HomePage from './Pages/HomePage';
+import ChatPage from './Pages/ChatPage';
 
 function App() {
   return (
     <div className="App">
-    <Route path='/' component={HomePage}/>
-    <Route path='/'/>
+    <Route path='/' component={HomePage} exact/>
+    <Route path='/chats' component={ChatPage} />
 
     </div>
   );
